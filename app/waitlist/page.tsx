2d6e31634847a9df2e8657db7fa1dc9e180f2b89
arosado-projects <anthonyrosado@gmail.com>
2026-05-24 18:49:11 -0500

    Update page.tsx

--- app/waitlist/page.tsx
+++ app/waitlist/page.tsx
@@ -1,5 +1,5 @@
 import { notFound } from "next/navigation";
-import WaitlistIntentWidget from "../../components/WaitlistIntentWidget";
+import WaitlistIntentWidget from "../components/WaitlistIntentWidget";
 
 export default function WaitlistPage() {
   if (process.env.WAITLIST_PAGE_ENABLED !== "true") {
