3ad15ff369a990461d0e8146aca5a2d270b8a41e
arosado-projects <anthonyrosado@gmail.com>
2026-05-30 06:37:55 -0500

    Update next.config.ts
    Add redirects

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/about-us",
+        destination: "/about",
+        permanent: true,
+      },
+      {
+        source: "/pricing-policies",
+        destination: "/pricing",
+        permanent: true,
+      },
+      {
+        source: "/services-schedule",
+        destination: "/services",
+        permanent: true,
+      },
+      {
+        source: "/sitemap",
+        destination: "/sitemap.xml",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
